daabaea7ed250cf5bec04a044751a0f074253f69
mfturkcan <bymehmet45tr@gmail.com>
2021-11-15 00:07:41 +0300

    Change file names

--- client/src/admin/AdminPanel.jsx
+++ client/src/admin/AdminPanel.jsx
@@ -2,25 +2,25 @@ import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
 import DataProvider from "./DataProvider";
 import IlcelerDuzenle from "./Ilceler/IlcelerDuzenle";
 import IlcelerEkle from "./Ilceler/IlcelerEkle";
-import IlcelerShow from "./Ilceler/IlcelerGöster";
+import IlcelerShow from "./Ilceler/IlcelerGoster";
 import IlcelerListe from "./Ilceler/IlcelerListe";
 import IllerDuzenle from "./Iller/IllerDuzenle";
 import IllerEkle from "./Iller/IllerEkle";
-import IllerShow from "./Iller/IllerGöster";
+import IllerShow from "./Iller/IllerGoster";
 import IllerListe from "./Iller/IllerListe";
 import KullaniciDuzenle from "./Kullanicilar/KullaniciDuzenle";
 import KullaniciEkle from "./Kullanicilar/KullaniciEkle";
-import KullaniciShow from "./Kullanicilar/KullaniciGöster";
+import KullaniciShow from "./Kullanicilar/KullaniciGoster";
 import KullaniciListe from "./Kullanicilar/KullaniciListe";
 import PersonelListe from "./Personel/PersonelListe";
 import BirimlerListe from "./Birimler/BirimlerListe";
 import PersonelEkle from "./Personel/PersonelEkle";
 import PersonelDuzenle from "./Personel/PersonelDuzenle";
 import BirimlerEkle from "./Birimler/BirimlerEkle";
-import BirimlerDüzenle from "./Birimler/BirimlerDüzenle";
+import BirimlerDuzenle from "./Birimler/BirimlerDuzenle";
 import MyLayout from "./Layout";
 import ProblemListe from './Problem/ProblemListe';
-import ProblemDuzenle from './Problem/ProblemDüzenle';
+import ProblemDuzenle from './Problem/ProblemDuzenle';
 import ProblemEkle from "./Problem/ProblemEkle";
 import AktiviteListe from './Problem/Aktivite/AktiviteListe';
 import AktiviteEkle from './Problem/Aktivite/AktiviteEkle';
@@ -95,7 +95,7 @@ const AdminPanel = props => {
             <Resource name="ciktidetay" options={{ label: "Çıktı Detay" }} list={CiktiDetayListe} create={CiktiDetayEkle} edit={CiktiDetayDuzenle} />
             <Resource name="mudahaledetay" options={{ label: "Müdahale Detay" }} list={MudahaleDetayListe} create={MudahaleDetayEkle} edit={MudahaleDetayDuzenle} />
             <Resource name="problembirim" options={{ label: "Problem Birim" }} list={ProblemBirimListe} create={ProblemBirimEkle} edit={ProblemBirimDuzenle} />
-            <Resource name="birimler" options={{ label: "Birimler" }} list={BirimlerListe} create={BirimlerEkle} edit={BirimlerDüzenle} />
+            <Resource name="birimler" options={{ label: "Birimler" }} list={BirimlerListe} create={BirimlerEkle} edit={BirimlerDuzenle} />
 
         </Admin>
     };
@@ -104,7 +104,7 @@ const AdminPanel = props => {
         return <Admin dataProvider={DataProvider} layout={MyLayout} theme={theme} loginPage={false} catchAll={NotFound} authProvider={authProvider}>
             <Resource name="kullanicilar" options={{ label: "Kullanıcılar" }} show={KullaniciShow} list={KullaniciListe} edit={KullaniciDuzenle} create={KullaniciEkle} />
             <Resource name="personel" options={{ label: "Personel" }} list={PersonelListe} create={PersonelEkle} edit={PersonelDuzenle} />
-            <Resource name="birimler" options={{ label: "Birimler" }} list={BirimlerListe} create={BirimlerEkle} edit={BirimlerDüzenle} />
+            <Resource name="birimler" options={{ label: "Birimler" }} list={BirimlerListe} create={BirimlerEkle} edit={BirimlerDuzenle} />
             <Resource name="iller" options={{ label: "İller" }} show={IllerShow} list={IllerListe} edit={IllerDuzenle} create={IllerEkle} />
             <Resource name="ilceler" options={{ label: "İlçeler" }} show={IlcelerShow} list={IlcelerListe} edit={IlcelerDuzenle} create={IlcelerEkle} />
             <Resource name="problem" options={{ label: "Problem" }} list={ProblemListe} create={ProblemEkle} edit={ProblemDuzenle} />
